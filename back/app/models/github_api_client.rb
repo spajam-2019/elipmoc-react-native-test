309e9150940a7f45f1c7329761e0a88f98e546c0
elipmoc <relipmoc101@gmail.com>
2019-04-12 01:42:39 +0900

    GitHubAPIClientの基本実装

--- back/app/models/github_api_client.rb
+++ back/app/models/github_api_client.rb
@@ -1,2 +1,25 @@
+require 'httpclient'
+
+BASE_URL = "https://api.github.com/"
+
 class GithubApiClient
+
+  def initialize
+    @http_client = HTTPClient.new
+    @http_client.base_url = BASE_URL
+  end
+
+  # github_api
+  def get_github_api_help
+    convert_response @http_client.get("/")
+  end
+
+  private
+
+  def convert_response res
+    if res.status != 200
+      pp ({error: res.headers})
+    end
+    JSON.parse res.body
+  end
 end
--- back/spec/models/github_api_client_spec.rb
+++ back/spec/models/github_api_client_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'Github Api Client' do
 
-  it 'hoge' do
-    GithubApiClient.new
-
+  it 'get_github_api_help' do
+    client = GithubApiClient.new
+    body = client.get_github_api_help
+    expect(body["current_user_url"]).to eq "https://api.github.com/user"
   end
 end
