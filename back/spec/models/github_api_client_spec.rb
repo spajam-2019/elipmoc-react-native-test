055f3aceca70912a930fc7307448b1db0ab4e450
elipmoc <relipmoc101@gmail.com>
2019-04-14 02:21:39 +0900

    RED リポジトリ検索の処理をテスト

--- back/spec/models/github_api_client_spec.rb
+++ back/spec/models/github_api_client_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 describe 'Github Api Client' do
+  client = GithubApiClient.new
 
   it 'get_github_api_help' do
-    client = GithubApiClient.new
     body = client.get_github_api_help
     expect(body["current_user_url"]).to eq "https://api.github.com/user"
+  end
+
+  it "リポジトリ検索" do
+    body = client.find_repositories "a"
+    expect(body["repos"]).to_not eq nil
+    expect(body["now_page"]).to eq 1
+    expect(body["page_count"]).to_not eq nil
+    expect(body["repos"][0]["id"]).to_not eq nil
+    expect(body["repos"][0]["name"]).to_not eq nil
+    expect(body["repos"][0]["lang"]).to_not eq nil
+    expect(body["repos"][0]["star"]).to_not eq nil
+    expect(body["repos"][0]["desc"]).to_not eq nil
   end
 end
